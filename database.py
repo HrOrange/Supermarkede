9d1ad1ed450c0a2952d296911ab0b5f0d3462c5e
Joaband <58936279+Joaband@users.noreply.github.com>
2021-01-08 19:53:01 +0100

    Joachim
    yesd yes

--- database.py
+++ database.py
@@ -1,6 +1,15 @@
-import sqlite3
-
 #laver nogle databaser
 #1. database til brugere
 #2. database til vare
 #3. database til så meget andet. føl .dia filen
+import math
+import random
+import time
+import sqlite3
+from flask import g
+import os
+
+class Data:
+    def __init__(self, navn):
+        self.con = sqlite3.connect(navn + ."db")
+        print("'Database åben'")
