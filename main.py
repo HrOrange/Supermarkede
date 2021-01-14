fa7f91298c753a9c6019af0fd3640e1030a1fa5d
HrOrange <nicolai.hansen.2000@hotmail.com>
2021-01-14 14:05:26 +0100

    main overhaul
    i have made a overhaul to the login/register window in main

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
+#tkinter imports
 import tkinter as tk
+#import tkinter.font as font
+
+#generic imports
 import time
 import random
 import utility #from another file
 import database #from another file
 
 #vare_data = Data("vare")
+
 
 ansatte_data = database.Data("ansatte", table_columns = ['navn STRING', 'løn REAL', 'fyret INT'], randoms = [['peter', 4.5, 0], ['harry', 1200.2, 1], ['zahir', -1, 0]])
 ansatte_data.print()
@@ -15,93 +20,92 @@ users_data.print()
 #vagter = database.Data("vagter")
 #kunde_data = database.Data("vare")
 
+#myFont = font.Font(family='Helvetica', size = 15, weight = "bold")
+class login_register_window(tk.Frame):
+    def __init__(s, master = None, window_size = [300, 200]):
+        tk.Frame.__init__(s, master)
 
-#Her åbner vi et login/registrer
+        #my_frame = tk.Frame(s, width = 300, height = 300)
+        #my_frame.place(width=200, height=200)
+        #s.root.protocol("WM_DELETE_WINDOW", lambda : s.root.destroy())
+
+        s.place(width = window_size[0], height = window_size[1])
+        s.got_in = False
+
+        #labels
+        s.name_label = tk.Label(s, text = "Name", font = ('Helvetica', '15', 'bold'))
+        s.name_label.place(relx = 0.17, rely = 0.25, width = window_size[0] * 0.6, height = window_size[1] * 0.15, anchor = tk.CENTER)
+        s.password_label = tk.Label(s, text = "Password", font = ('Helvetica', '15', 'bold'))
+        s.password_label.place(relx = 0.17, rely = 0.4, width = window_size[0] * 0.6, height = window_size[1] * 0.15, anchor = tk.CENTER)
+
+        #entries
+        s.name_entry = tk.Entry(s, font = ('Helvetica', '15', 'bold'))
+        s.name_entry.place(relx = 0.65, rely = 0.25, width = window_size[0] * 0.6, height = window_size[1] * 0.15, anchor = tk.CENTER)
+        s.password_entry = tk.Entry(s, font = ('Helvetica', '15', 'bold'))
+        s.password_entry.place(relx = 0.65, rely = 0.4, width = window_size[0] * 0.6, height = window_size[1] * 0.15, anchor = tk.CENTER)
+
+        #buttons
+        button_size = [200, 50]
+        s.login_button = tk.Button(s, text="Login", command = s.login, font = ('Helvetica', '15', 'bold'))
+        s.login_button.place(relx = 0.5, rely = 0.65, width = window_size[0] * 0.8, height = window_size[1] * 0.2, anchor = tk.CENTER)
+        s.registrer_button = tk.Button(s, text="Registrer", command = s.registrer, font = ('Helvetica', '15', 'bold'))
+        s.registrer_button.place(relx = 0.5, rely = 0.85, width = window_size[0] * 0.8, height = window_size[1] * 0.2, anchor = tk.CENTER)
+
+    def login(s):
+        name = s.name_entry.get()
+        password = s.password_entry.get()
+
+        s.got_in = users_data.check_user(name, password)
+
+        if s.got_in:
+            s.close()
+        else:
+            print('Nope')
+    def registrer(s):
+        name = s.name_entry.get()
+        password = s.password_entry.get()
+
+        role = "boss"
+        s.got_in = users_data.check_user(name, password)
+
+        if s.got_in:
+            name_entry.set('')
+            password_entry.set('')
+        else:
+            s.close()
+    def close(s):
+        s.destroy()
+
+role = None
 window_size = [300, 200]
 root = tk.Tk()
 root.geometry(str(window_size[0]) + "x" + str(window_size[1]))
 root.title("Login/Registrer")
+login_window = login_register_window(root)
+login_window.mainloop()
 
-from tkinter import messagebox
-def on_closing():
-    #if messagebox.askokcancel("Quit", "Do you want to quit?"):
-    root.destroy()
+print(login_window.got_in)
 
-root.protocol("WM_DELETE_WINDOW", on_closing)
-
-# define font
-import tkinter.font as font
-myFont = font.Font(family='Helvetica', size = 15, weight = "bold")
-
-check = False
-
-def login():
-    name = name_entry.get()
-    password = password_entry.get()
-    print(name)
-    print(password)
-
-    global user_role
-    user_role = "boss"
-    #TODO: login
-
-    check = users_data.check_user(name, password)
-
-    if(check):
-        root.destroy()
-    else:
-        print('Nope')
-        None
-
-def registrer():
-    #TODO: register
-    if(True):
-        root.destroy()
-    else:
-        name_entry.set('')
-        password.set('')
-
-#labels
-name_label = tk.Label(root, text = "Name", font = myFont)
-name_label.place(relx = 0.17, rely = 0.25, width = window_size[0] * 0.6, height = window_size[1] * 0.15, anchor = tk.CENTER)
-password_label = tk.Label(root, text = "Password", font = myFont)
-password_label.place(relx = 0.17, rely = 0.4, width = window_size[0] * 0.6, height = window_size[1] * 0.15, anchor = tk.CENTER)
-
-#entries
-name_entry = tk.Entry(root, font = myFont)
-name_entry.place(relx = 0.65, rely = 0.25, width = window_size[0] * 0.6, height = window_size[1] * 0.15, anchor = tk.CENTER)
-password_entry = tk.Entry(root, font = myFont)
-password_entry.place(relx = 0.65, rely = 0.4, width = window_size[0] * 0.6, height = window_size[1] * 0.15, anchor = tk.CENTER)
-
-#buttons
-button_size = [200, 50]
-login_button = tk.Button(root, text="Login", command = login, font = myFont)
-login_button.place(relx = 0.5, rely = 0.65, width = window_size[0] * 0.8, height = window_size[1] * 0.2, anchor = tk.CENTER)
-registrer_button = tk.Button(root, text="Registrer", command = registrer, font = myFont)
-registrer_button.place(relx = 0.5, rely = 0.85, width = window_size[0] * 0.8, height = window_size[1] * 0.2, anchor = tk.CENTER)
-
-root.mainloop() #the pause in code
-
-if check:
+"""if login_window.check:
     #Her åbner vi main programmet
     window_size = [600, 400]
     originial_window_size = [window_size[0], window_size[1]]
-    root = tk.Tk()
-    root.geometry(str(window_size[0]) + "x" + str(window_size[1]))
-    root.title("Supermarket")
-    myFont = font.Font(family='Helvetica', size = 15, weight = "bold")
+    s.root = tk.Tk()
+    s.root.geometry(str(window_size[0]) + "x" + str(window_size[1]))
+    s.root.title("Supermarket")
+    s.myFont = font.Font(family='Helvetica', size = 15, weight = "bold")
 
     def window_resize_event(event):
-        myFont = font.Font(family='Helvetica', size = int(20 * (0.4 * root.winfo_width() / originial_window_size[0] + 0.6 * root.winfo_height() / originial_window_size[1])), weight = "bold")
+        s.myFont = font.Font(family='Helvetica', size = int(20 * (0.4 * s.root.winfo_width() / originial_window_size[0] + 0.6 * s.root.winfo_height() / originial_window_size[1])), weight = "bold")
         for i in buttons:
-            buttons[i]['font'] = myFont
-    root.bind('<Configure>', window_resize_event) #this calls the function window_resize_event whenever a window resize event is happening
+            buttons[i]['font'] = s.myFont
+    s.root.bind('<Configure>', window_resize_event) #this calls the function window_resize_event whenever a window resize event is happening
 
     #frames
-    right_frame = tk.Frame(root, bg = 'red', width = window_size[0] * 0.6, height = window_size[1])
+    right_frame = tk.Frame(s.root, bg = 'red', width = window_size[0] * 0.6, height = window_size[1])
     top_right_frame = tk.Frame(right_frame, bg = 'pink', width = window_size[0] * 0.6, height = window_size[1] * 0.6)
     bottom_right_frame = tk.Frame(right_frame, bg = 'orange', width = window_size[0] * 0.6, height = window_size[1] * 0.4)
-    left_frame = tk.Frame(root, bg = 'blue', width = window_size[0] * 0.4, height = window_size[1])
+    left_frame = tk.Frame(s.root, bg = 'blue', width = window_size[0] * 0.4, height = window_size[1])
 
     left_frame.pack(side = tk.LEFT, expand = True, fill = tk.BOTH)
     right_frame.pack(side = tk.LEFT, expand = True, fill = tk.BOTH)
@@ -160,7 +164,7 @@ if check:
         texts = ['Giv Rolle', 'Rediger Rolle', 'Rediger Pris', 'Vagt Oversigt']
         funcs = [give_role, edit_role, edit_pris, shift_overview]
         for i in range(len(texts)):
-            buttons[variable_names[i]] = tk.Button(left_frame, text = texts[i], command = funcs[i], font = myFont)
+            buttons[variable_names[i]] = tk.Button(left_frame, text = texts[i], command = funcs[i], font = s.myFont)
             buttons[variable_names[i]].place(relx = 0.5, rely = (i + 0.5) / len(texts), relwidth = 1, relheight = 1 / len(texts), anchor = tk.CENTER)
 
         #give_role_button = tk.Button(left_frame, text = "Giv Rolle", command = give_role).pack(side = tk.TOP)
@@ -168,5 +172,5 @@ if check:
         #edit_price_button = tk.Button(left_frame, text = "Rediger Pris", command = edit_pris).pack(side = tk.TOP)
         #shift_overview_button = tk.Button(left_frame, text = "Vagt Oversigt", command = shift_overview).pack(side = tk.TOP)
 
-    root.mainloop()
+    s.root.mainloop()"""
     #objekter i programmet
